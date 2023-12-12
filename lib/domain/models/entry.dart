bb21d99e4f11f366851d6c9a1904037f80b9e159
Moises Correa <moisesbrambilla@hotmail.com>
2023-12-12 11:23:09 -0300

    Entendendo sobre a camada de dados e utilitarios

--- lib/domain/models/entry.dart
+++ lib/domain/models/entry.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:floor/floor.dart';
+
+@entity
 class Entry {
+  @primaryKey
   int id;
   String name;
   String category;
